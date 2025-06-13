ca0220dc3f21de5fafd6a23107821488006ee8b7
g0at1 <michal.lendzion@vp.pl>
2025-06-14 00:21:22 +0200

    App shouldn't appear in dock

--- speedometer/SystemMonitorApp.swift
+++ speedometer/SystemMonitorApp.swift
@@ -1,7 +1,15 @@
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+    }
+}
 
 @main
 struct SystemMonitorApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     var body: some Scene {
         MenuBarExtra("Monitor", image: "monitorIcon") {
             ContentView()
